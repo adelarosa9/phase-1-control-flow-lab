03fb7f9123519abc3c57634e946b91869e813f9a
alejandro delarosa <a.delarosa9@yahoo.com>
2023-12-14 15:21:58 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  // 1) gives customers a free sample if the ride is less than or equal to 400 feet
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  // 2) charges 20 dollars for a distance between 400 and 2000 feet
+  else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  }
+  // 3) charges 30 dollars for a distance over 2000 feet
+  else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  // 4) does not allow rides over 2500 feet
+  else if (distance > 2500) {
+    return "No can do.";
+  }
+}
+// Function for ternaryCheckCity
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function for switchOnCharmFromTip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
